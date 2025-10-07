abafea687dde9b282b76657d17db88391dae952a
jiezou <x1991093603@163.com>
2025-10-07 21:19:20 +0800

    Add files via upload
    第一个小游戏结束了。。。

---
+++ 猜数字.c
@@ -0,0 +1,9 @@
+﻿生成开始于 21:18...
+1>------ 已启动生成: 项目: learn, 配置: Debug x64 ------
+1>D:\Visual Suudio 2022\MSBuild\Microsoft\VC\v170\Microsoft.CppBuild.targets(538,5): warning MSB8028: 中间目录(x64\Debug\)包含从另一个项目(Project1.vcxproj)共享的文件。   这会导致错误的清除和重新生成行为。
+1>learn3.c
+1>C:\Users\JieZou\source\repos\Project1\Project1\learn3.c(13,15): warning C4244: “函数”: 从“time_t”转换到“unsigned int”，可能丢失数据
+1>Project1.vcxproj -> C:\Users\JieZou\source\repos\Project1\x64\Debug\learn.exe
+1>已完成生成项目“Project1.vcxproj”的操作。
+========== 生成: 1 成功，0 失败，0 最新，0 已跳过 ==========
+========== 生成 于 21:18 完成，耗时 00.311 秒 ==========
